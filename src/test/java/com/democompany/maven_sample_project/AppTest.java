ba5a5b65ace51504404e9cb4b754453ad4b8de52
sureshtalari <mailtosuresh.bi@gmail.com>
2022-03-10 22:08:08 +0530

    added initial commit

---
+++ src/test/java/com/democompany/maven_sample_project/AppTest.java
@@ -0,0 +1,20 @@
+package com.democompany.maven_sample_project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
